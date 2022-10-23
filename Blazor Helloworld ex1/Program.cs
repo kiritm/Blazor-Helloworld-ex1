621fe26c4ece6af8becce02cecd7fc44f6b9f11c
kelvinshandy <100505548+kelvinshandy@users.noreply.github.com>
2022-10-23 20:39:39 +0800

    Add string to view in browser

--- Blazor Helloworld ex1/Program.cs
+++ Blazor Helloworld ex1/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
+string strMessage = "\nHello World" + "\nThis sample shows string concatation using \'+\' operator.";
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
